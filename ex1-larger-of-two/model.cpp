bd3f62905ccfa8bece39fcc032e2ecc4643f12d9
mosothoanepitso3-oss <mosothoanepitso3@gmail.com>
2026-09-11 12:53:46 +0200

    Update model.cpp

--- ex1-larger-of-two/model.cpp
+++ ex1-larger-of-two/model.cpp
@@ -1,1 +1,17 @@
-C++ model
+#include <iostream>
+
+int main() {
+    int a = 15;
+    int b = 42;
+    int max;
+
+    if (a >= b) {
+        max = a;
+    } else {
+        max = b;
+    }
+
+    std::cout << max << std::endl;
+    return 0;
+}
+
